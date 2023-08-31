a8a78a6df320c1c6baac4643bb4f420500f16d07
Paige Black <125614142+Techprenuer1@users.noreply.github.com>
2023-08-31 14:11:12 +0000

    updated one to many relationships

--- src/models.py
+++ src/models.py
@@ -13,16 +13,17 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    favorite_characters = Column(String(250))
-    favorite_planet = Column(String(250))
-    favorite_vehicle = Column(String(250))
+    favorites = relationship("Favorites", backref="user")
 
+    
+  
 class Favorites(Base):
     __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    favorite_characters = Column(String(250), ForeignKey('characters.name'))
-    favorite_planet = Column(String(250), ForeignKey('planet.name'))
-    favorite_vehicle = Column(String(250), ForeignKey('vehicle.name'))
+    favorite_characters = Column(Integer(250), ForeignKey('characters.id'))
+    favorite_planet = Column(Integer(250), ForeignKey('planet.id'))
+    favorite_vehicle = Column(Integer(250), ForeignKey('vehicle.id'))
+    user_id = Column(Integer(250)), ForeignKey('user.id')
 
 
 
@@ -38,8 +39,8 @@ class Character(Base):
     skin_color = Column(String(250))
     gender = Column(String(250))
     birth_year = Column(Integer)  
-    homeworld = Column(String(250))  
-    person_id = Column(Integer, ForeignKey('person.id'))
+    homeworld = Column(String(250)) 
+    favorite = relationship('favorites', backref="characters") 
     # person = relationship(Person)
 
 
@@ -51,6 +52,7 @@ class Planet(Base):
     population = Column(Integer)
     terrain = Column(String(250))
     rotation_period = Column(Integer)
+    favorite = relationship('favorites', backref='planet')
     
 
 class Vehicle(Base):
@@ -62,6 +64,7 @@ class Vehicle(Base):
     cost_in_credits = Column(Integer)
     passengers = Column(Integer)
     starship_class = Column(String(250))
+    favorite = relationship('favorites', backref='vehicle')
 
 
 
